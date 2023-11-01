42b39f8b176e7bac6205b5464e0de2e2a4343830
rajak0549 <97539723+rajak0549@users.noreply.github.com>
2023-11-01 17:55:39 +0530

    Add files via upload
    RainbowSchoolData uploaded

---
+++ DataStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainbowSchoolData
+{
+    internal class DataStorage
+    {
+        private static DataStorage instance;
+
+        private DataStorage() { }
+
+        public static DataStorage Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataStorage();
+
+
+                }
+                return instance;
+            }
+        }
+
+        public List<Student> Students { get; } = new List<Student>();
+        public List<Teacher> Teachers { get; } = new List<Teacher>();
+        public List<Subject> Subjects { get; } = new List<Subject>();
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainbowSchoolData
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+    }
+
+    // Teacher.cs
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+    }
+
+    // Subject.cs
+    public class Subject
+    {
+        public string Name { get; set; }
+        public string SubjectCode { get; set; }
+        public Teacher Teacher { get; set; }
+
+    }
+}
